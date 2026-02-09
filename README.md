8587ef8335944ef51c2539699db17e4649a88297
Samman2 <shakyasamman92@gmail.com>
2026-02-09 13:39:47 +0545

    automation without otp verification

--- README.md
+++ README.md
@@ -1,13 +1,36 @@
 # Automated_Signup
+First step clone the repo in your computer 
+    git clone https://github.com/Samman2/Automated_Signup.git
 
-/* Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-PS C:\Users\Samman\OneDrive\Desktop\Automated_Signup> py -m venv venv
->>
->> # 3. Activate the environment (You should see (venv) appear in green)
->> .\venv\Scripts\Activate.ps1
->>
->> # 4. Install the required libraries
->> pip install playwright pytest-playwright requests
->>
->> # 5. Install the browser drivers
->> playwright install chromium*/
+Download python 
+    Python 3.14.3 
+    py --version 
+    To check python is installed successfully 
+
+Second step open the folder in your editor
+In terminal 
+    Create and setup enviroment
+    python -m venv venv # Create
+    venv\Scripts\activate # Setup 
+
+    Install playwright 
+    pip install playwright pytest-playwright requests
+
+    Install Chromium for Broswer
+    playwright install chromium
+
+    To run the program 
+    pytest -s --headed --slowmo 500 signup_automation_script.py
+
+
+#Note 
+    # Otp verification cannot be automated as the otp was sent to the user's phone no and its a manual process
+    # ( Correction : not in user's email as the website says Email verification code ) 
+
+
+Tools and Devices
+OS: Windows 11
+Python Version: 3.x
+Browser: Chromium
+IDE: VS Code
+Git 
--- signup_automation_script.py
+++ signup_automation_script.py
@@ -4,37 +4,39 @@ import time
 import requests
 from playwright.sync_api import Page, expect
 
-# --- CONFIGURATION ---
-MAIL7_DOMAIN = "mail7.io"
-USERNAME = f"agent_{int(time.time())}"
-EMAIL = f"{USERNAME}@{MAIL7_DOMAIN}"
+MAIL_DOMAIN = "gmail.com"
+EMAIL = f"{test}@{MAIL_DOMAIN}"
 
 def test_full_signup_flow(page: Page):
-
+#Navigation 
     page.goto("https://authorized-partner.vercel.app/")
     page.get_by_role("button", name="Join Us Now").first.click()
     
-
+# Case : Continue without I agree checkbox
     page.get_by_role("button", name="Continue").click()
 
-    
     page.get_by_role("checkbox").check()
     page.get_by_role("button", name="Continue").click()
 
-    page.get_by_placeholder("Enter Your First Name").fill("Python")
+# Filling data 
+    page.get_by_placeholder("Enter Your First Name").fill("Automation")
     page.get_by_placeholder("Enter Your Last Name").fill("Tester")
     page.get_by_placeholder("Enter Your Email Address").fill(EMAIL)
-    
     page.get_by_placeholder("00-00000000").fill("9711111111")
     
-
+# Case : Checking password mismatched
     password_fields = page.locator('input[type="password"]')
-    password_fields.nth(0).fill("SecurePass123!")
-    password_fields.nth(1).fill("WrongPass123!")
+    password_fields.nth(0).fill("S123456789!")
+    password_fields.nth(1).fill("S987654321!")
     page.get_by_role("button", name="Next").click()
   
-    password_fields.nth(1).fill("SecurePass123!")
+    # Correction 
+    password_fields.nth(1).fill("S123456789!")
     page.get_by_role("button", name="Next").click()
+    
+    #Note 
+    # Otp verification cannot be automated as the otp was sent to the user's phone no and its a manual process
+    # ( Correction : not in user's email as the website says Email verification code ) 
     
     print("\n>>> PAUSE: Check your phone for the OTP.")
     otp_code = input("Enter the 6-digit OTP from your phone: ")
